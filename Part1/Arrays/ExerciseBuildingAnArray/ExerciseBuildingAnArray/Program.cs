b88a384c809aaf7a011213309b2d74333075f5c3
Tuna <32073253+Tunairl@users.noreply.github.com>
2024-06-23 18:09:04 +0300

    Created a dynamic array class.
    Wrote the functions; insert(), removeAt(), indexOf(), and print().
    
    insert(): adds a new item to the array. If the array is full, it creates a new array twice the size and copies the former values. Then sets the reference between the former and the latter.
    
    removeAt(): removes the element at the given index. Shifts the other values by 1 to the left.
    
    indexOf(): returns the number of the index of a given element.
    
    print(): prints the array elements using a for loop.

---
+++ Part1/Arrays/ExerciseBuildingAnArray/ExerciseBuildingAnArray/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.CompilerServices;
+
+partial class Program
+{
+
+    static void Main(string[] args)
+    {
+        var numbers = new Array(4);
+        numbers.insert(10);
+        numbers.insert(20);
+        numbers.insert(30);
+        numbers.insert(40);
+        numbers.insert(50);
+
+
+        Console.WriteLine(numbers.indexOf(10));
+        Console.WriteLine(numbers.indexOf(15));
+        numbers.print();
+
+    }
+}
