4bd32f088e47909e4a10ce16439180fb63834f9f
Tuna <32073253+Tunairl@users.noreply.github.com>
2024-06-27 09:31:03 +0300

    Add files via upload

---
+++ Part1/LinkedListExercise/LinkedListExercise/LinkedList.cs
@@ -0,0 +1,66 @@
+﻿public class LinkedList
+{
+    private Node first;
+    private Node last;
+    private class Node
+    {
+        private int value;
+        private Node next;
+
+        public Node(int value)
+        {
+            this.value = value;
+        }
+
+        private Node Next { get => next; set => next = value; }
+
+        public void setNextNode(Node node) { // Çözüm
+            this.next = node;
+        }
+    }
+
+    public void addLast(int item)
+    {
+        Node node = new Node(item);
+
+        // Check if the list is empty
+        if (first == null)
+        {
+            first = last = node;
+        }
+        else
+        {
+            last.setNextNode(node); // Hatayı aldığım satır "last.next = node;"
+            last = node;
+        }
+    }
+
+    
+
+    // addFirst
+    // addLast
+    // deleteFirst
+    // deleteLast
+    // contains
+    // indexOf
+
+    public void addFirst(int item)
+    {
+        if (first != null)
+        {
+            first.setNextNode() 
+        }
+    }
+
+    public void print()
+    {
+        
+    }
+
+    //public void addFirst(int value)
+    //{
+    //    Node newNode = new Node(value);
+
+    //    newNode.
+    //}
+}
---
+++ Part1/LinkedListExercise/LinkedListExercise/Program.cs
@@ -0,0 +1,12 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        var list = new LinkedList();
+        list.addLast(10);
+        list.addLast(20);
+        list.addLast(30);
+
+        
+    }
+}
