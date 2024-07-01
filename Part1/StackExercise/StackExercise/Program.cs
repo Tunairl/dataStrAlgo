0b9f65885ef7417fbf70a1ca1b44d2712c3880cb
Tuna <32073253+Tunairl@users.noreply.github.com>
2024-07-01 11:50:28 +0300

    Add files via upload

---
+++ Part1/StackExercise/StackExercise/Program.cs
@@ -0,0 +1,43 @@
+﻿
+class StringReverser 
+{
+    public string Reverse(string str)
+    {
+        if (str == null)
+        {
+            throw new ArgumentNullException();
+        }
+
+        Stack<char> stack = new Stack<char>();
+
+        //String str = "abcd";
+
+        foreach (var ch in str.ToCharArray())
+        {
+            stack.Push(ch);
+        }
+
+        var reversed = new System.Text.StringBuilder();
+        while (stack.Count() != 0)
+        {
+            reversed.Append(stack.Pop());
+        }
+
+        return reversed.ToString();
+
+    }
+}
+
+class Program 
+{
+    static void Main(string[] args)
+    {
+        StringReverser reverser = new StringReverser();
+        var str = "mehmet mi";
+        var result = reverser.Reverse("hello world");
+        Console.WriteLine(result);
+        Console.WriteLine(reverser.Reverse(str));
+
+    }
+
+}
