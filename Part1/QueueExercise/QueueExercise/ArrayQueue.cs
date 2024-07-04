4c6da8f354698be3e823dd0d4eb2a9055b97a6e2
Tuna <32073253+Tunairl@users.noreply.github.com>
2024-07-04 17:21:10 +0300

    Add files via upload

---
+++ Part1/QueueExercise/QueueExercise/ArrayQueue.cs
@@ -0,0 +1,62 @@
+﻿
+
+class ArrayQueue
+{
+    private int Front;
+    private int Rear;
+    private int count;
+    private int[] items;
+
+    public ArrayQueue(int size)
+    {
+        items = new int[size];
+    }
+
+    public void Enqueue(int item)
+    {
+        if (count == items.Length)
+        {
+            throw new OverflowException();
+        }
+
+        items[Rear] = item;
+        Rear = (Rear + 1) % items.Length;
+        count++;
+        
+    }
+
+    public int Dequeue()
+    {
+        var item = items[Front];
+        items[Front] = 0;
+        Front = (Front + 1) % items.Length;
+        count--;
+        return item;
+    }
+
+    public int Peek()
+    {
+        return items[Front];
+    }
+
+    public void Print()
+    {
+        foreach (var item in items)
+        {
+            Console.Write(item + " ");
+        }
+    }
+
+    public bool IsEmpty()
+    {
+        return count == 0;
+    }
+
+    public bool IsFull()
+    {
+        return count == items.Length;
+    }
+
+}
+
+
---
+++ Part1/QueueExercise/QueueExercise/Program.cs
@@ -0,0 +1,61 @@
+﻿partial class Program
+{
+    static void Main(string[] args)
+    {
+        Queue<int> que = new Queue<int>();
+
+        que.Enqueue(10);
+        que.Enqueue(20);
+        que.Enqueue(30);
+
+        //foreach (var item in queue)
+        //{
+        //    Console.WriteLine(item);
+        //}
+
+        //var front = queue.Dequeue();
+
+        //Console.WriteLine(front);
+
+        //foreach (var item in queue)
+        //{
+        //    Console.WriteLine(item);
+        //} 
+
+        //Reverse(que);
+
+        //foreach (var item in que)
+        //{
+        //    Console.WriteLine(item);
+        //}
+
+
+        var queue = new ArrayQueue(3);
+
+        queue.Enqueue(10);
+        queue.Enqueue(20);
+        queue.Enqueue(30);
+        queue.Dequeue();
+        queue.Enqueue(40);
+        queue.Dequeue();
+        queue.Dequeue();
+        queue.Enqueue(50);
+        queue.Print();
+
+    }
+
+    public static void Reverse(Queue<int> queue)
+    {
+        var stack = new Stack<int>();
+
+        while (queue.Count() != 0)
+        {
+            stack.Push(queue.Dequeue());
+        }
+        while (stack.Count() != 0)
+        {
+            queue.Enqueue(stack.Pop());
+        }
+    }
+}
+
