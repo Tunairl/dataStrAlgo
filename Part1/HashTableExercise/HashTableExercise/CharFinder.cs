5adb4f70f1cdb94ff2c05d48689f65499a1ba1e2
Tuna <32073253+Tunairl@users.noreply.github.com>
2024-07-17 10:44:55 +0300

    Implemented a function using Dictionary<T>
    Wrote a function that finds and returns the first non-repeating character in a string.

---
+++ Part1/HashTableExercise/HashTableExercise/CharFinder.cs
@@ -0,0 +1,25 @@
+﻿public class CharFinder
+{
+    public char FindFirstNonRepeatingChar(string str)
+    {
+        var charDict = new Dictionary<char, int>();
+        int count = 0;
+        var chars = str.ToCharArray();
+        foreach (var ch in chars)
+        {
+            if (charDict.Keys.Contains(ch))
+            {
+                count = charDict[ch];
+                charDict[ch] = count + 1;
+                continue;
+            }
+            charDict.Add(ch, 1);
+        }
+
+        if (charDict.Values.Contains(1))
+        {
+            return charDict.First(x => x.Value == 1).Key;
+        }
+        return Char.MinValue;
+    }
+}
---
+++ Part1/HashTableExercise/HashTableExercise/Program.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        // Key: Employee Number (int)
+        // Value: Name (string)
+
+        Dictionary<int, string> dict = new Dictionary<int, string>();
+        dict.Add(1, "Tuna");
+        dict.Add(2, "Lily");
+        dict.Add(3, "Nora");
+
+        //foreach (var kvp in dict)
+        //{
+        //    Console.WriteLine("Key: {0}, Value: {1}", kvp.Key, kvp.Value);
+        //}
+
+        //var myKey = dict.FirstOrDefault(x => x.Value == "Lily").Key;
+
+        dict.ContainsKey(2); // O(1)
+        dict.ContainsValue("Nora"); // O(n)
+
+        //Console.WriteLine(myKey);
+
+        CharFinder finder = new CharFinder();
+
+        Console.WriteLine(finder.FindFirstNonRepeatingChar("aabb"));
+    }
+}
