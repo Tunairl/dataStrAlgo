93a55693ba5e4dce8ad2c1fc39b2f4a4ed67767a
Tuna <32073253+Tunairl@users.noreply.github.com>
2024-07-22 16:46:33 +0300

    Add files via upload

--- Part1/HashTableExercise/HashTableExercise/CharFinder.cs
+++ Part1/HashTableExercise/HashTableExercise/CharFinder.cs
@@ -14,7 +14,7 @@
                 continue;
             }
             charDict.Add(ch, 1);
-        }
+        } 
 
         if (charDict.Values.Contains(1))
         {
@@ -23,7 +23,53 @@
         return Char.MinValue;
     }
 
+    public char FindFirstNonRepeatedChar(string str)
+    {
+        var dict = new Dictionary<char, int>();
+
+        int count = 0;
+        var chars = str.ToCharArray();
+
+        foreach (var ch in chars)
+        {
+            if (dict.ContainsKey(ch))
+            {
+                count = dict[ch];
+                dict[ch] = count + 1;
+                continue;
+            }
+            dict.Add(ch, 1);
+        }
+
+        if (dict.ContainsValue(1))
+        {
+            return dict.First(x => x.Value == 1).Key;
+        }
+
+        return Char.MinValue;
+    }
+
     public char FindFirstRepeatedChar(string str)
+    {
+        var set = new HashSet<char>();
+
+        var chars = str.ToCharArray();
+
+        for (int i = 0; i < chars.Length; i++)
+        {
+            if (set.Contains(chars[i]))
+            {
+                return chars[i];
+            }
+
+            set.Add(chars[i]);
+
+        }
+
+        return Char.MinValue;
+    }
+
+    public char FindFirstRepeatingChar(string str)
     {
         var set = new HashSet<char>();
         var chars = str.ToCharArray();
@@ -36,8 +82,38 @@
             }
 
             set.Add(chars[i]);
-            continue;
         }
         return Char.MinValue;
+    }
+
+    public int MostFrequent(int[] nums)
+    {
+        var dict = new Dictionary<int, int>();
+        int count = 0;
+
+        foreach (var num in nums)
+        {
+            if (dict.ContainsKey(num))
+            {
+                count = dict[num];
+                dict[num] = count + 1;
+                continue;
+            }
+            dict.Add(num, 0);
+        }
+
+        int max = -1;
+        int result = nums[0];
+
+        foreach (var item in dict)
+        {
+            if (item.Value > max)
+            {
+                max = item.Value;
+                result = item.Key;
+            }
+        }
+
+        return result;
     }
 }
--- Part1/HashTableExercise/HashTableExercise/Program.cs
+++ Part1/HashTableExercise/HashTableExercise/Program.cs
@@ -2,19 +2,74 @@
 {
     static void Main(string[] args)
     {
-        HashSet<int> set = new HashSet<int>();
-        int[] numbers = { 1, 2, 3, 3, 2, 1, 4 };
-        foreach (var number in numbers)
-        {
-            set.Add(number);
-        }
+        //Dictionary<string, string> dict = new Dictionary<string, string>();
+        //dict.Add("123456-A", "Tuna");
 
-        //Console.WriteLine(String.Join(",", set));
+        //Console.WriteLine(hash("123456-A"));
 
-        var finder = new CharFinder();
+        CharFinder charFinder = new CharFinder();
 
-        var ch = finder.FindFirstRepeatedChar("green apple");
-        Console.WriteLine(ch);
+        var a = new int[] { 1, 2, 2, 5, 3, 3, 3, 4, 5, 5, 5, 3, 3};
+
+        Console.WriteLine(charFinder.FindFirstNonRepeatedChar("tunatron"));
+        Console.WriteLine(charFinder.FindFirstRepeatedChar("tunatron"));
+
+        Console.WriteLine(charFinder.MostFrequent(a));
 
     }
+
+    public static int hash(string key)
+    {
+        int hash = 0;
+        foreach (var ch in key.ToCharArray())
+        {
+            hash += ch;
+        }
+        return hash;
+    }
+}
+
+class HashTable
+{
+    private class Entry
+    {
+        private int key;
+        private string value;
+
+        public Entry(int key, string value)
+        {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private LinkedList<Entry>[] entries = new LinkedList<Entry>[5];
+
+    //public void Put(int key, string value)
+    //{
+    //    var index = Hash(key);
+    //    if (entries[index] == null)
+    //    {
+    //        entries[index] = new LinkedList<Entry>();
+    //    }
+
+    //    var bucket = entries[index];
+
+    //    foreach (Entry entry in bucket)
+    //    {
+    //        if (entry.key == key)
+    //        {
+    //            entry.value = value;
+    //            return;
+    //        }
+    //    }
+         
+    //    bucket.AddLast(new Entry(key, value));
+    ////}
+
+    private int Hash(int key)
+    { // Math.abs(...);
+        return key % entries.Length;
+    }
+
 }
